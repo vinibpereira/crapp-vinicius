2e8ac768d3c3a0e024e74381f315fada13502a8b
Vinicius Barenho Pereira <viniciusbarenho@gmail.com>
2019-11-06 14:09:34 -0300

    Updates

---
+++ precache-manifest.d49199d79c0ab81cc97d1d012320d34d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94fa7c5a1ff20ac49344c897f489d83",
+    "url": "/crapp-vinicius/index.html"
+  },
+  {
+    "revision": "439e74b378dde7f55a62",
+    "url": "/crapp-vinicius/static/css/main.c7e98262.chunk.css"
+  },
+  {
+    "revision": "e67c23c52914b7fca30f",
+    "url": "/crapp-vinicius/static/js/2.b28c58c9.chunk.js"
+  },
+  {
+    "revision": "439e74b378dde7f55a62",
+    "url": "/crapp-vinicius/static/js/main.aaeeb55f.chunk.js"
+  },
+  {
+    "revision": "22d3bffd55af25948d7d",
+    "url": "/crapp-vinicius/static/js/runtime-main.efdfb356.js"
+  }
+]);
